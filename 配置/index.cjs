7124718ccfa5366889cc4fb9088e6d854a624bb6
liangxiran <114726619+liangxiran@users.noreply.github.com>
2022-09-30 16:03:53 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '送给可爱的夏夏老婆',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'cfKBtY9kqfeHcbSNrOsAy4m5XMDhqoYdcx05ZqZiOfs',
       // 所在省份
       province: '江苏',
       // 所在城市
